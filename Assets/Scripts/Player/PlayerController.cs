0797f7467b564c48d26f283afd0623e277d0fcf8
Ebru <ebru_cakar_11@hotmail.com>
2023-03-07 22:58:52 +0300

    Project init with 8. video.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ZenvaSurvival.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Transform cameraContainer;
+        public float minXLook, maxXLook;
+        private float camCurXRot;
+        public float lookSensitivity;
+
+        private Vector2 mouseDelta;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void LateUpdate()
+        {
+            CameraLook();
+        }
+
+        void CameraLook()
+        {
+            camCurXRot += mouseDelta.y * lookSensitivity;
+            camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+            cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+            transform.eulerAngles += new Vector3(0, mouseDelta.x - lookSensitivity, 0);
+        }
+
+        public void OnLookInput(InputAction.CallbackContext context)
+        {
+            mouseDelta = context.ReadValue<Vector2>();
+        }
+    }
+}
