645430793e1b2c1e68488694c0796260c1d79c60
Ebru <ebru_cakar_11@hotmail.com>
2023-03-08 23:04:57 +0300

    Lesson 14 completed.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,54 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace ZenvaSurvival.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        public Transform cameraContainer;
+        [Header("Move")] public float moveSpeed;
+        private Vector2 curMovementInput;
+        public float jumpForce;
+        public LayerMask groundLayerMask;
+
+        [Header("Look")] public Transform cameraContainer;
         public float minXLook, maxXLook;
         private float camCurXRot;
         public float lookSensitivity;
 
         private Vector2 mouseDelta;
 
+        //components
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
         }
 
+        private void Update()
+        {
+            Move();
+        }
+
         private void LateUpdate()
         {
             CameraLook();
+        }
+
+        void Move()
+        {
+            Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+            dir *= moveSpeed;
+            dir.y = _rigidbody.velocity.y;
+
+            _rigidbody.velocity = dir;
         }
 
         void CameraLook()
@@ -29,12 +57,65 @@ namespace ZenvaSurvival.Player
             camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
             cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
 
-            transform.eulerAngles += new Vector3(0, mouseDelta.x - lookSensitivity, 0);
+            transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
         }
 
         public void OnLookInput(InputAction.CallbackContext context)
         {
             mouseDelta = context.ReadValue<Vector2>();
+        }
+
+        public void OnMoveInput(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                curMovementInput = context.ReadValue<Vector2>();
+            }
+            else if (context.phase == InputActionPhase.Canceled)
+            {
+                curMovementInput = Vector2.zero;
+            }
+        }
+
+        public void OnJumpInput(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Started)
+            {
+                if (IsGrounded())
+                {
+                    _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                }
+            }
+        }
+
+        bool IsGrounded()
+        {
+            Ray[] rays = new Ray[4]
+            {
+                new Ray(transform.position + (transform.forward * .2f) + (Vector3.up * .01f), Vector3.down),
+                new Ray(transform.position + (-transform.forward * .2f) + (Vector3.up * .01f), Vector3.down),
+                new Ray(transform.position + (transform.right * .2f) + (Vector3.up * .01f), Vector3.down),
+                new Ray(transform.position + (-transform.right * .2f) + (Vector3.up * .01f), Vector3.down)
+            };
+
+            for (int i = 0; i < rays.Length; i++)
+            {
+                if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position + (transform.forward * .2f), Vector3.down);
+            Gizmos.DrawRay(transform.position + (-transform.forward * .2f), Vector3.down);
+            Gizmos.DrawRay(transform.position + (transform.right * .2f), Vector3.down);
+            Gizmos.DrawRay(transform.position + (-transform.right * .2f), Vector3.down);
         }
     }
 }
---
+++ Assets/Scripts/Player/PlayerNeeds.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ZenvaSurvival.Player
+{
+    public class PlayerNeeds : MonoBehaviour
+    {
+        public Need health, hunger, thirst, sleep;
+
+        public float noHungerhealthDecay, noThirstHealthDecay;
+
+        public UnityEvent onTakeDamage;
+
+        private void Start()
+        {
+            //set start values
+            health.curValue = health.startValue;
+            hunger.curValue = hunger.startValue;
+            thirst.curValue = thirst.startValue;
+            sleep.curValue = sleep.startValue;
+        }
+
+        private void Update()
+        {
+            //decay need over time
+            hunger.Subtract(hunger.decayRate * Time.deltaTime);
+            thirst.Subtract(thirst.decayRate * Time.deltaTime);
+            sleep.Add(sleep.regenRate * Time.deltaTime);
+
+            if (hunger.curValue == 0)
+                health.Subtract(noHungerhealthDecay * Time.deltaTime);
+            if (thirst.curValue == 0)
+                health.Subtract(noThirstHealthDecay * Time.deltaTime);
+
+            if (health.curValue == 0)
+            {
+                Die();
+            }
+
+            //update ui bars
+            health.uiBar.fillAmount = health.GetPercentage();
+            hunger.uiBar.fillAmount = hunger.GetPercentage();
+            thirst.uiBar.fillAmount = thirst.GetPercentage();
+            sleep.uiBar.fillAmount = sleep.GetPercentage();
+        }
+
+        public void Heal(float amount)
+        {
+            health.Add(amount);
+        }
+        
+        public void Eat(float amount)
+        {
+            hunger.Add(amount);
+        }
+        
+        public void Drink(float amount)
+        {
+            thirst.Add(amount);
+        }
+        
+        public void Sleep(float amount)
+        {
+            sleep.Subtract(amount);
+        }
+
+        public void TakePhysicalDamage(int amount)
+        {
+            health.Subtract(amount);
+            onTakeDamage?.Invoke();
+        }
+
+        public void Die()
+        {
+            Debug.Log("Player is dead");
+        }
+    }
+
+    [System.Serializable]
+    public class Need
+    {
+        [HideInInspector] public float curValue;
+        public float maxValue;
+        public float startValue;
+        public float regenRate;
+        public float decayRate;
+        public Image uiBar;
+
+        public void Add(float amount)
+        {
+            curValue = Mathf.Min(curValue + amount, maxValue);
+        }
+
+        public void Subtract(float amount)
+        {
+            curValue = Mathf.Max(curValue - amount, 0);
+        }
+
+        public float GetPercentage()
+        {
+            return curValue / maxValue;
+        }
+    }
+}
